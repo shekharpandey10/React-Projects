0168e83f999161c0dc20d6f75608f249751eefc9
Chandra shekhar pandey <158845488+shekharpandey10@users.noreply.github.com>
2025-05-14 17:05:00 +0530

    final commit of theme-switcher app

--- theme-switcher/src/App.jsx
+++ theme-switcher/src/App.jsx
@@ -1,15 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
+import { ThemeProvider } from './context/Theme'
+import ThemeButton from './components/ThemeButton'
+import Card from './components/Card'
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [themeMode, setThemeMode] = useState("light")
+
+  const lightTheme=()=>{
+    setThemeMode("light")
+  }
+  const darkTheme=()=>{
+    setThemeMode("dark")
+  }
+
+//actual change in theme
+
+useEffect(() => {
+  const html = document.querySelector('html');
+  if (html) {
+    html.classList.remove("light", "dark");
+    html.classList.add(themeMode);
+  }
+}, [themeMode]);
+
 
   return (
-    <>
-     <h1 className='bg-pink-500 p-4 text-2xl'>Hello</h1>
-    </>
+    <ThemeProvider value={{themeMode,lightTheme,darkTheme}}>
+    <div className='flex flex-wrap min-h-screen items-center'>
+      <div className='w-full'>
+        <div className='w-full max-w-sm mx-auto flex justify-end mb-4'>
+          <ThemeButton />
+        </div>
+
+        <div className='w-full max-w-sm mx-auto'>
+          <Card />
+        </div>
+      </div>
+    </div>
+    </ThemeProvider>
   )
 }
 
